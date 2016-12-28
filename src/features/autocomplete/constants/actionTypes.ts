07175b751d03e85c2dee40450a198a6c7d267c83
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-28 14:34:55 +0300

    Add action type

--- src/features/autocomplete/constants/actionTypes.ts
+++ src/features/autocomplete/constants/actionTypes.ts
@@ -1,6 +1,7 @@
 const actionTypes = {
   INPUT: 'autocomplete/INPUT',
   REQUEST: 'autocomplete/REQUEST',
+  REQUEST_TIME_UPDATE: 'autocomplete/REQUEST_TIME_UPDATE',
   RESPONSE_SUCCESS: 'autocomplete/RESPONSE_SUCCESS',
   RESPONSE_FAILURE: 'autocomplete/RESPONSE_FAILURE',
 };
