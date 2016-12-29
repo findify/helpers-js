19872781d758744dd0e9cc163293b6d6a5eef503
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-29 22:18:03 +0300

    Inject reduxStore dependency into createAutocomplete function

--- src/features/autocomplete/index.ts
+++ src/features/autocomplete/index.ts
@@ -1,4 +1,5 @@
 import * as FindifySDK from 'findify-sdk';
+import * as Redux from 'redux';
 
 import {
   StateName,
@@ -28,6 +29,7 @@ import {
   getSuggestions,
   getQuery,
   getMeta,
+  State as ReduxState,
 } from './reducers';
 
 import { rootReducer } from './reducers';
@@ -41,111 +43,117 @@ import { configureReduxStore } from '../../generic/helpers/configureReduxStore';
 
 // avoid names duplication between redux state/store and lib state/store
 
-function createAutocomplete(config: Config): Store<EmitEvent, SubscribeEvent, StateName, StateResult> {
-  if (config.user && !isExists(config.user.uid)) {
-    throw new Error('"user.uid" param is required');
-  }
+function makeCreateAutocomplete(reduxStore: Redux.Store<ReduxState>) {
+  function createAutocomplete(config: Config): Store<EmitEvent, SubscribeEvent, StateName, StateResult> {
+    if (config.user && !isExists(config.user.uid)) {
+      throw new Error('"user.uid" param is required');
+    }
 
-  if (config.user && !isExists(config.user.sid)) {
-    throw new Error('"user.sid" param is required');
-  }
+    if (config.user && !isExists(config.user.sid)) {
+      throw new Error('"user.sid" param is required');
+    }
 
-  const sdk = FindifySDK.init(config);
+    const sdk = FindifySDK.init(config);
 
-  return {
-    emit(event: EmitEvent) {
-      if (!isExists(event)) {
-        throw new Error('Please, specify event you want to emit');
-      }
-
-      if (event.name === eventsNames.input && (
-        !isExists(event.payload) || !isExists((event as InputEvent).payload.query)
-      )) {
-        throw new Error('"query" param is required in "input" event');
-      }
-
-      if (event.name === eventsNames.request && isExists(event.payload)) {
-        if (!isExists((event as RequestEvent).payload.user.uid)) {
-          throw new Error('"user.uid" param is required');
+    return {
+      emit(event: EmitEvent) {
+        if (!isExists(event)) {
+          throw new Error('Please, specify event you want to emit');
         }
 
-        if (!isExists((event as RequestEvent).payload.user.sid)) {
-          throw new Error('"user.sid" param is required');
+        if (event.name === eventsNames.input && (
+          !isExists(event.payload) || !isExists((event as InputEvent).payload.query)
+        )) {
+          throw new Error('"query" param is required in "input" event');
         }
-      }
 
-      if (event.name === eventsNames.request && !isExists(config.user) && (
-        !isExists(event.payload) || !isExists((event as RequestEvent).payload.user)
-      )) {
-        throw new Error('`user` param should be provided either at request or at library config');
-      }
+        if (event.name === eventsNames.request && isExists(event.payload)) {
+          if (!isExists((event as RequestEvent).payload.user.uid)) {
+            throw new Error('"user.uid" param is required');
+          }
 
-      switch (event.name) {
-        case eventsNames.input:
-          reduxStore.dispatch(input({
-            query: (event as InputEvent).payload.query,
-          }));
-          break;
-        case eventsNames.request:
-          reduxStore.dispatch(request({
-            itemsLimit: (event as RequestEvent).payload.itemsLimit,
-            suggestionsLimit: (event as RequestEvent).payload.suggestionsLimit,
-            user: (event as RequestEvent).payload.user,
-          }, sdk));
-          break;
-      }
+          if (!isExists((event as RequestEvent).payload.user.sid)) {
+            throw new Error('"user.sid" param is required');
+          }
+        }
 
-      return this;
-    },
+        if (event.name === eventsNames.request && !isExists(config.user) && (
+          !isExists(event.payload) || !isExists((event as RequestEvent).payload.user)
+        )) {
+          throw new Error('`user` param should be provided either at request or at library config');
+        }
 
-    subscribe(listener: SubscribeListener<SubscribeEvent>) {
-      if (typeof listener !== 'function') {
-        throw new Error('Listener should be a function');
-      }
-
-      return reduxStore.subscribe(() => {
-        const action = getLastAction(reduxStore.getState());
-
-        switch (action.type) {
-          case actionTypes.INPUT:
-            listener(createEvent<InputEvent>(eventsNames.input, {
-              query: (action as InputAction).payload.query,
+        switch (event.name) {
+          case eventsNames.input:
+            reduxStore.dispatch(input({
+              query: (event as InputEvent).payload.query,
             }));
             break;
-          case actionTypes.REQUEST:
-            listener(createEvent<RequestEvent>(eventsNames.request, {
-              itemsLimit: (action as RequestAction).payload.itemsLimit,
-              suggestionsLimit: (action as RequestAction).payload.suggestionsLimit,
-              user: (action as RequestAction).payload.user,
-            }));
-            break;
-          case actionTypes.RESPONSE_SUCCESS:
-            listener(createEvent<ResponseSuccessEvent>(eventsNames.responseSuccess));
-            break;
-          case actionTypes.RESPONSE_FAILURE:
-            listener(createEvent<ResponseFailureEvent>(eventsNames.responseFailure));
+          case eventsNames.request:
+            reduxStore.dispatch(request({
+              itemsLimit: (event as RequestEvent).payload.itemsLimit,
+              suggestionsLimit: (event as RequestEvent).payload.suggestionsLimit,
+              user: (event as RequestEvent).payload.user,
+            }, sdk));
             break;
         }
-      });
-    },
 
-    get(name: StateName) {
-      if (!isExists(stateNames[name])) {
-        throw new Error('Event not found');
-      }
-      // !!!
-      // don't forget about cases when we need to return value by one name but from different
-      // sources(from request or either from response)
+        return this;
+      },
 
-      const state = reduxStore.getState();
+      subscribe(listener: SubscribeListener<SubscribeEvent>) {
+        if (!isExists(listener)) {
+          throw new Error('Please, provide listener function');
+        }
 
-      switch (name) {
-        case stateNames.products: return runSafe(() => getProducts(state));
-        case stateNames.suggestions: return runSafe(() => getSuggestions(state));
-        case stateNames.query: return runSafe(() => getQuery(state));
-        case stateNames.meta: return runSafe(() => getMeta(state));
-      }
-    },
+        if (typeof listener !== 'function') {
+          throw new Error('Listener should be a function');
+        }
+
+        return reduxStore.subscribe(() => {
+          const action = getLastAction(reduxStore.getState());
+
+          switch (action.type) {
+            case actionTypes.INPUT:
+              listener(createEvent<InputEvent>(eventsNames.input, {
+                query: (action as InputAction).payload.query,
+              }));
+              break;
+            case actionTypes.REQUEST:
+              listener(createEvent<RequestEvent>(eventsNames.request, {
+                itemsLimit: (action as RequestAction).payload.itemsLimit,
+                suggestionsLimit: (action as RequestAction).payload.suggestionsLimit,
+                user: (action as RequestAction).payload.user,
+              }));
+              break;
+            case actionTypes.RESPONSE_SUCCESS:
+              listener(createEvent<ResponseSuccessEvent>(eventsNames.responseSuccess));
+              break;
+            case actionTypes.RESPONSE_FAILURE:
+              listener(createEvent<ResponseFailureEvent>(eventsNames.responseFailure));
+              break;
+          }
+        });
+      },
+
+      get(name: StateName) {
+        if (!isExists(stateNames[name])) {
+          throw new Error('Event not found');
+        }
+        // !!!
+        // don't forget about cases when we need to return value by one name but from different
+        // sources(from request or either from response)
+
+        const state = reduxStore.getState();
+
+        switch (name) {
+          case stateNames.products: return runSafe(() => getProducts(state));
+          case stateNames.suggestions: return runSafe(() => getSuggestions(state));
+          case stateNames.query: return runSafe(() => getQuery(state));
+          case stateNames.meta: return runSafe(() => getMeta(state));
+        }
+      },
+    };
   };
 }
 
@@ -157,6 +165,7 @@ function createEvent<E>(name, payload?): E {
 }
 
 const reduxStore = configureReduxStore(rootReducer, rootSaga);
+const createAutocomplete = makeCreateAutocomplete(reduxStore);
 
 type EmitEvent = (
   InputEvent |
