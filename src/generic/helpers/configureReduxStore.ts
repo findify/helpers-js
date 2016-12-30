00ab53b26a7628cc15cee1b2ffa592da533235e6
Ivan Mironchik <me.roaiven@gmail.com>
2016-12-30 16:28:30 +0300

    Remove logger

--- src/generic/helpers/configureReduxStore.ts
+++ src/generic/helpers/configureReduxStore.ts
@@ -1,14 +1,11 @@
-import * as createLogger from 'redux-logger';
 import * as Redux from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 function configureReduxStore(rootReducer, rootSaga) {
-  const logger = process.env.NODE_ENV === 'development' && typeof window !== 'undefined' && createLogger();
   const sagaMiddleware = createSagaMiddleware();
 
   const middlewares = [
     sagaMiddleware,
-    logger,
   ].filter(Boolean);
 
   const store = Redux.createStore(
