944580722af58aaf59f9c8d1e2b0f95585ae3d6d
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-26 17:14:34 +0300

    Add search constants

---
+++ src/features/search/constants/eventsNames.ts
@@ -0,0 +1,17 @@
+const eventsNames = {
+  search: 'search',
+  setPage: 'setPage',
+  nextPage: 'nextPage',
+  prevPage: 'prevPage',
+  changeSorting: 'changeSorting',
+  toggleNestedListFacet: 'toggleNestedListFacet',
+  toggleTextFacet: 'toggleTextFacet',
+  toggleRangeFacet: 'toggleRangeFacet',
+  request: 'request',
+  responseSuccess: 'responseSuccess',
+  responseFailure: 'responseFailure',
+};
+
+export {
+  eventsNames,
+};
