0ae0a26ded7ffed3cd0d54bc8c2e513f918ed82c
Ivan Mironchik <me.roaiven@gmail.com>
2017-01-27 11:33:44 +0300

    Define public interface of search

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,13 @@ declare module "@findify/findify-helpers" {
   type Unsubscribe = () => void;
   type User = FindifySDK.User;
 
+  type ResponseSuccessEvent = {
+    name: 'responseSuccess',
+  };
+  type ResponseFailureEvent = {
+    name: 'responseFailure',
+  };
+
   type AutocompleteServerRequest = FindifySDK.AutocompleteRequest;
   type AutocompleteServerResponse = FindifySDK.AutocompleteResponse;
 
@@ -22,22 +29,15 @@ declare module "@findify/findify-helpers" {
       user?: User,
     },
   };
-  type AutocompleteResponseSuccessEvent = {
-    name: 'responseSuccess',
-  };
-  type AutocompleteResponseFailureEvent = {
-    name: 'responseFailure',
-  };
   type AutocompleteEmitEvent = (
     AutocompleteInputEvent |
     AutocompleteRequestEvent
   );
 
   type AutocompleteSubscribeEvent = (
-    AutocompleteInputEvent |
-    AutocompleteRequestEvent |
-    AutocompleteResponseSuccessEvent |
-    AutocompleteResponseFailureEvent
+    AutocompleteEmitEvent |
+    ResponseSuccessEvent |
+    ResponseFailureEvent
   );
   type AutocompleteSubscribeListener = (event: AutocompleteSubscribeEvent) => void;
   type AutocompleteStore = {
@@ -48,4 +48,81 @@ declare module "@findify/findify-helpers" {
   };
 
   function createAutocomplete(config: Config): AutocompleteStore;
+
+  type ResultsNextPageEvent = {
+    name: 'nextPage',
+  };
+  type ResultsPrevPageEvent = {
+    name: 'prevPage',
+  };
+  type ResultsSetPageEvent = {
+    name: 'setPage',
+    payload: {
+      page: number,
+    },
+  };
+  type ResultsChangeSortingEvent = {
+    name: 'changeSorting',
+    payload: {
+      field: string,
+      order: string,
+    },
+  };
+  type ResultsToggleNestedListFacetEvent = {
+    name: 'toggleNestedListFacet',
+    payload: {
+      name: string,
+      value: string,
+    },
+  };
+  type ResultsToggleTextFacetEvent = {
+    name: 'toggleTextFacetEvent',
+    payload: {
+      name: string,
+      value: string,
+    },
+  };
+  type ResultsToggleRangeFacetEvent = {
+    name: 'toggleRangleFacetEvent',
+    payload: {
+      name: string,
+      value: string,
+    },
+  };
+
+  type SearchServerRequest = FindifySDK.SearchRequest;
+  type SearchServerResponse = FindifySDK.SearchResponse;
+
+  type SearchNextPageEvent = ResultsNextPageEvent;
+  type SearchPrevPageEvent = ResultsPrevPageEvent;
+  type SearchSetPageEvent = ResultsSetPageEvent;
+  type SearchChangeSortingEvent = ResultsChangeSortingEvent;
+  type SearchToggleNestedListFacetEvent = ResultsToggleNestedListFacetEvent;
+  type SearchToggleTextFacetEvent = ResultsToggleTextFacetEvent;
+  type SearchToggleRangeFacetEvent = ResultsToggleRangeFacetEvent;
+
+  type SearchEmitEvent = (
+    SearchNextPageEvent |
+    SearchPrevPageEvent |
+    SearchSetPageEvent |
+    SearchChangeSortingEvent |
+    SearchToggleNestedListFacetEvent |
+    SearchToggleTextFacetEvent |
+    SearchToggleRangeFacetEvent |
+  );
+  type SearchSubscribeEvent = (
+    SearchEmitEvent |
+    ResponseSuccessEvent |
+    ResponseFailureEvent
+  );
+
+  type SearchSubscribeListener = (event: SearchSubscribeEvent) => void;
+  type SearchStore = {
+    emit: (event: SearchEmitEvent) => SearchStore,
+    subscribe(listener: SearchSubscribeListener): Unsubscribe,
+    get(name: 'request'): SearchServerRequest,
+    get(name: 'response'): SearchServerResponse,
+  };
+
+  function createSearch(config: Config): SearchStore;
 }
