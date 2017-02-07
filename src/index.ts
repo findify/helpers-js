37ed4a56eb4f7c042c108696ceaf27088336f50b
Ivan Mironchik <me.roaiven@gmail.com>
2017-02-07 16:14:22 +0300

    Exporting recommendation feature

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import 'babel-polyfill';
 export { create as createAutocomplete } from './features/autocomplete';
 export { create as createSearch } from './features/search';
 export { create as createCollection } from './features/collection';
+export { create as createRecommendation } from './features/recommendation';
