f4bd53780a078b0c7a29dba4f0d07703a208ec05
Ivan Mironchik <me.roaiven@gmail.com>
2017-02-08 15:00:40 +0300

    Fix validation condition

--- src/features/recommendation/validations.ts
+++ src/features/recommendation/validations.ts
@@ -18,11 +18,11 @@ function init(type: FindifySDK.RecommendationsType) {
 }
 
 function emit(type: FindifySDK.RecommendationsType, event: Event) {
-  if (type === 'predefined' && !has(event.payload, 'slot')) {
+  if (type === 'predefined' && event.name === 'request' && !has(event.payload, 'slot')) {
     throw new Error('"slot" param is required');
   }
 
-  if ((type === 'viewed' || type === 'bought') && !has(event.payload, 'item_id')) {
+  if ((type === 'viewed' || type === 'bought') && event.name === 'request' && !has(event.payload, 'item_id')) {
     throw new Error('"item_id" param is required');
   }
 }
