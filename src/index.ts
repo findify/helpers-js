43c2b355d6d41fa4ed5781fed55a80b6ffa63eb5
Ivan Mironchik <me.roaiven@gmail.com>
2017-04-06 15:06:29 +0300

    Replace babel-polyfill by plugin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import 'babel-polyfill';
-
 export { create as createAutocomplete } from './features/autocomplete';
 export { create as createSearch } from './features/search';
 export { create as createCollection } from './features/collection';
